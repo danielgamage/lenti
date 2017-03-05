15ed43141ff7cb2a057b434e39ff0eb5496510a0
Daniel Gamage <hello@danielgamage.com>
2017-03-05 04:22:35 -0500

    prepping for multi-image effect

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,32 +3,44 @@ var Lenticula = function (options) {
 
   // Config
   this.container = options.container
+  this.imageData
 
-  // Init
+  // Initialize
   this.init = () => {
     _this.images = [..._this.container.querySelectorAll(`img`)]
+    _this.imageData = [..._this.images]
     _this.container.innerHTML += `<canvas />`
     _this.canvas = _this.container.querySelector(`canvas`)
     _this.ctx = _this.canvas.getContext(`2d`)
-    const imageWidth = 720
-    const imageHeight = 480
-    let width = _this.canvas.clientWidth
-    let height = _this.canvas.clientHeight
+    _this.canvas.setAttribute(`width`, `500`)
+    _this.canvas.setAttribute(`height`, `500`)
 
-    _this.canvas.addEventListener("click", () => { _this.redraw(width, height) })
+    _this.images.map((image, imageIndex) => {
+      let htmlImg = new Image()
+      htmlImg.addEventListener('load', function() {
+        console.log('loaded')
+        _this.ctx.drawImage(htmlImg, 0, 0, htmlImg.naturalWidth, htmlImg.naturalHeight, 0, 0, _this.canvas.clientWidth, _this.canvas.clientHeight)
+        _this.imageData[imageIndex] = _this.ctx.getImageData(0, 0, _this.canvas.clientWidth, _this.canvas.clientHeight)
+      }, false);
+      htmlImg.crossOrigin = "Anonymous"
+      htmlImg.src = image.src
+    })
+
+    _this.canvas.addEventListener("click", () => { _this.redraw() })
   }
-  this.redraw = (width, height) => {
-      _this.ctx.drawImage(_this.images[0], 0, 0, 700, 500, 0, 0, width, height)
-      const imageData = _this.ctx.getImageData(0,0,width, height)
 
-      console.log(imageData)
-      let data = imageData.data
+  // Redraw canvas
+  this.redraw = () => {
+    console.log(_this.imageData)
+    const imageData = _this.ctx.getImageData(0, 0, _this.canvas.clientWidth, _this.canvas.clientHeight)
 
-      // only works if width is even num
-      for (var i = 0; i < data.length; i += 8) {
-        data[i + 3] = 0; // alpha
-      }
-      _this.ctx.putImageData(imageData, 0, 0, 0, 0, width, height)
+    let data = imageData.data
+
+    // only works if width is even num
+    for (var i = 0; i < data.length; i += 8) {
+      data[i + 3] = 0; // alpha
+    }
+    _this.ctx.putImageData(imageData, 0, 0, 0, 0, _this.canvas.clientWidth, _this.canvas.clientHeight)
   }
 }
 
