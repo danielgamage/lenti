7ac0bc37b9a2a36f5323f8ea6006d449d38ec263
Daniel Gamage <hello@danielgamage.com>
2017-03-06 01:33:03 -0500

    using es6 classes

--- src/index.js
+++ src/index.js
@@ -1,98 +1,115 @@
 /* global window */
-let Lenti = function (options) {
-  let _this = this
+class Lenti {
+  constructor(options) {
+    // Config
+    this.container = options.container
+    this.accelerometerEvents = options.accelerometerEvents || true
+    this.mouseEvents = options.mouseEvents || true
+    this.stripWidth = options.stripWidth || 16
+    this.height = options.height || 50
+    this.width = options.width || 50
 
-  // Config
-  this.container = options.container
-  this.accelerometerEvents = options.accelerometerEvents || true
-  this.mouseEvents = options.mouseEvents || true
-  this.stripWidth = options.stripWidth || 16
-  this.height = options.height || 50
-  this.width = options.width || 50
+    this.bindEvents = this.bindEvents.bind(this)
+    this.init = this.init.bind(this)
+    this.getImage = this.getImage.bind(this)
+    this.handleResize = this.handleResize.bind(this)
+    this.bindEvents = this.bindEvents.bind(this)
+    this.destroy = this.destroy.bind(this)
+    this.redraw = this.redraw.bind(this)
+    this.handleMouse = this.handleMouse.bind(this)
+    this.handleOrientation = this.handleOrientation.bind(this)
+    this.remap = this.remap.bind(this)
+  }
 
   // Initialize
-  this.init = () => {
-    _this.images = [..._this.container.querySelectorAll(`img`)]
-    _this.imageDataArray = [..._this.images]
-    _this.container.innerHTML += `<canvas />`
-    _this.canvas = _this.container.querySelector(`canvas`)
-    _this.ctx = _this.canvas.getContext(`2d`)
-    _this.canvas.setAttribute(`width`, _this.width)
-    _this.canvas.setAttribute(`height`, _this.height)
-    _this.canvasWidth = _this.canvas.clientWidth
-    _this.canvasHeight = _this.canvas.clientHeight
-    _this.imageData = _this.ctx.getImageData(0, 0, _this.canvasWidth, _this.canvasHeight)
+  init() {
+    this.images = [...this.container.querySelectorAll(`img`)]
+    this.imageDataArray = [...Array(this.images.length).keys()] // empty array w/ same length as this.images
+    this.container.innerHTML += `<canvas />`
+    this.canvas = this.container.querySelector(`canvas`)
+    this.ctx = this.canvas.getContext(`2d`)
+    this.canvas.setAttribute(`width`, this.width)
+    this.canvas.setAttribute(`height`, this.height)
+    this.canvasWidth = this.canvas.clientWidth
+    this.canvasHeight = this.canvas.clientHeight
+    this.imageData = this.ctx.getImageData(0, 0, this.canvasWidth, this.canvasHeight)
 
-    _this.images.map((image, imageIndex) => {
+    this.images.map((imageEl, imageIndex) => {
       let htmlImg = new window.Image()
-      htmlImg.addEventListener(`load`, function () {
-        console.log(`loaded`)
-        _this.ctx.drawImage(htmlImg, 0, 0, htmlImg.naturalWidth, htmlImg.naturalHeight, 0, 0, _this.canvasWidth, _this.canvas.clientHeight)
-        const currImageData = _this.ctx.getImageData(0, 0, _this.canvasWidth, _this.canvas.clientHeight)
-        _this.imageDataArray[imageIndex] = JSON.parse(JSON.stringify(currImageData))
-      }, false)
+      htmlImg.addEventListener(`load`, function() {
+        this.getImage(imageEl, imageIndex)
+      }.bind(this))
       htmlImg.crossOrigin = `Anonymous`
-      htmlImg.src = image.src
-      return true
+      htmlImg.src = imageEl.src
+      return imageEl
     })
 
-    _this.bindEvents()
+    this.bindEvents()
+  }
+
+  getImage(image, imageIndex) {
+    this.ctx.drawImage(image, 0, 0, image.naturalWidth, image.naturalHeight, 0, 0, this.canvasWidth, this.canvas.clientHeight)
+    const currImageData = this.ctx.getImageData(0, 0, this.canvasWidth, this.canvas.clientHeight)
+    this.imageDataArray[imageIndex] = JSON.parse(JSON.stringify(currImageData))
   }
 
   // Handle window resize
-  this.handleResize = () => {
-    _this.canvasWidth = _this.canvas.clientWidth
-    _this.canvasHeight = _this.canvas.clientHeight
+  handleResize() {
+    this.canvasWidth = this.canvas.clientWidth
+    this.canvasHeight = this.canvas.clientHeight
   }
 
   // Event Binding
-  this.bindEvents = () => {
-    if (_this.mouseEvents) {
-      _this.canvas.addEventListener(`mousemove`, _this.handleMouse)
+  bindEvents() {
+    if (this.mouseEvents) {
+      this.canvas.addEventListener(`mousemove`, this.handleMouse)
     }
-    if (_this.accelerometerEvents) {
-      window.addEventListener(`deviceorientation`, _this.handleOrientation)
+    if (this.accelerometerEvents) {
+      window.addEventListener(`deviceorientation`, this.handleOrientation)
     }
-    window.addEventListener(`resize`, _this.handleResize)
+    window.addEventListener(`resize`, this.handleResize)
   }
 
   // Event Unbinding
-  this.destroy = () => {
-    _this.canvas.addEventListener(`mousemove`, _this.handleMouse)
-    window.addEventListener(`deviceorientation`, _this.handleOrientation)
-    window.removeEventListener(`resize`, _this.handleResize)
+  destroy() {
+    this.canvas.addEventListener(`mousemove`, this.handleMouse)
+    window.addEventListener(`deviceorientation`, this.handleOrientation)
+    window.removeEventListener(`resize`, this.handleResize)
   }
 
   // Redraw canvas
-  this.redraw = balance => {
-    let data = _this.imageData.data
+  redraw(balance) {
+    // make sure data is loaded before redrawing
+    if (this.imageDataArray[0]) {
+      let data = this.imageData.data
 
-    for (let i = 0; i < data.length; i += 4) {
-      const set = (i / 4 % _this.canvasWidth % _this.stripWidth / _this.stripWidth) + (balance * _this.images.length) - 0.5
-      const setClamped = Math.floor(Math.min(Math.max(set, 0), _this.images.length - 1))
+      for (let i = 0; i < data.length; i += 4) {
+        const set = (i / 4 % this.canvasWidth % this.stripWidth / this.stripWidth) + (balance * this.images.length) - 0.5
+        const setClamped = Math.floor(Math.min(Math.max(set, 0), this.images.length - 1))
 
-      data[i + 0] = _this.imageDataArray[setClamped].data[i + 0] // r
-      data[i + 1] = _this.imageDataArray[setClamped].data[i + 1] // g
-      data[i + 2] = _this.imageDataArray[setClamped].data[i + 2] // b
-      data[i + 3] = _this.imageDataArray[setClamped].data[i + 3] // a
+        data[i + 0] = this.imageDataArray[setClamped].data[i + 0] // r
+        data[i + 1] = this.imageDataArray[setClamped].data[i + 1] // g
+        data[i + 2] = this.imageDataArray[setClamped].data[i + 2] // b
+        data[i + 3] = this.imageDataArray[setClamped].data[i + 3] // a
+      }
+      this.ctx.putImageData(this.imageData, 0, 0, 0, 0, this.canvasWidth, this.canvasHeight)
     }
-    _this.ctx.putImageData(_this.imageData, 0, 0, 0, 0, _this.canvasWidth, _this.canvasHeight)
   }
 
   // Handle mouse events
-  this.handleMouse = e => {
-    _this.redraw(e.offsetX / _this.canvasWidth)
+  handleMouse(e) {
+    this.redraw(e.offsetX / this.canvasWidth)
   }
 
   // Handle device accelerometer events
-  this.handleOrientation = e => {
+  handleOrientation(e) {
     const clamped = Math.max(Math.min(e.gamma, 45), -45)
-    const balance = _this.remap(clamped, -45, 45, 0, 1)
-    _this.redraw(balance)
+    const balance = this.remap(clamped, -45, 45, 0, 1)
+    this.redraw(balance)
   }
 
   // Map values from one range to another
-  this.remap = (value, inLow, inHigh, outLow, outHigh) => {
+  remap(value, inLow, inHigh, outLow, outHigh) {
     return outLow + ((value - inLow) * (outHigh - outLow) / (inHigh - inLow))
   }
 }
