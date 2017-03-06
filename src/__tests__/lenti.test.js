cea2baba68885181ea29cd8795e3df6e49f50a3c
Daniel Gamage <hello@danielgamage.com>
2017-03-06 01:33:14 -0500

    cleanup

--- src/__tests__/lenti.test.js
+++ src/__tests__/lenti.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert')
 const Lenti = require('../index.js')
 
-test('', () => {
-  expect(Lenti.remap(5, 0, 10, 50, 100)).toBe(75)
-})
+// test('', () => {
+//   expect(Lenti.remap(5, 0, 10, 50, 100)).toBe(75)
+// })
